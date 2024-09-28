56e5e547f7d7ff05840616b93af9890911c0cbd9
Hemel <reaz.hemel@gmail.com>
2024-09-29 00:28:03 +0600

    Dependecnies added

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+
+}
+buildscript{
+
+    dependencies {
+        val nav_version = "2.8.1"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
